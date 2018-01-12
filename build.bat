a1961cf3bb63137d4495d3cfec620667e0df29c6
Fernando Yurisich <fyurisich@oohg.org>
2018-01-12 20:25:08 -0300

    2018-01-12 20:25 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
       * ide\ChangeLog.txt
         * Updated.
       * ide\build.bat
       * ide\compile.bat
         * Deduce HG_ROOT from .bat's 0 parameter if not set.
       * ide\.gitignore
         + Some files.

--- build.bat
+++ build.bat
@@ -3,37 +3,44 @@ rem
 rem $Id: build.bat $
 rem
 
-:MAIN
+:BUILD_IDE
 
    cls
-   if /I "%1"=="/C" goto CLEAN_PATH
-   if "%HG_ROOT%"=="" set HG_ROOT=c:\oohg
+
+   pushd "%~dp0"
+   set HG_START_DP_IDE_BUILD_BAT=%CD%
+   popd
+
    set HG_CLEAN=
-   goto PARAMS
-
-:CLEAN_PATH
-
-   set HG_ROOT=c:\oohg
-   set HG_CLEAN=/C
+   if /I not "%1" == "/C" goto ROOT
    shift
+   set HG_CLEAN=/C
+   set HG_ROOT=
 
-:PARAMS
+:ROOT
+
+   if not "%HG_ROOT%" == "" goto TEST
+   pushd "%HG_START_DP_IDE_BUILD_BAT%\.."
+   set HG_ROOT=%CD%
+   popd
+
+:TEST
 
    if /I "%1"=="HB30" ( shift & goto CALL30 )
    if /I "%1"=="HB32" ( shift & goto CALL32 )
 
 :NOVERSION
 
-   if exist %HG_ROOT%\buildapp30.bat goto CONTINUE
-   if exist %HG_ROOT%\buildapp32.bat goto CALL32
-   echo File %HG_ROOT%\buildapp30.bat not found !!!
-   echo File %HG_ROOT%\buildapp32.bat not found !!!
+   if exist "%HG_ROOT%\BuildApp30.bat" goto CONTINUE
+   if exist "%HG_ROOT%\BuildApp32.bat" goto CALL32
+   echo File %HG_ROOT%\BuildApp30.bat not found !!!
+   echo File %HG_ROOT%\BuildApp32.bat not found !!!
    echo.
    goto END
 
 :CONTINUE
 
-   if not exist %HG_ROOT%\buildapp32.bat goto CALL30
+   if not exist "%HG_ROOT%\BuildApp32.bat" goto CALL30
    echo Syntax:
    echo    To build with Harbour 3.0
    echo       build [/C] HB30 [options]
@@ -44,12 +51,15 @@ rem
 
 :CALL30
 
-   call %HG_ROOT%\buildapp.bat %HG_CLEAN% HB30 mgide %1 %2 %3 %4 %5 %6 %7 %8 %9
+   call "%HG_ROOT%\BuildApp.bat" %HG_CLEAN% HB30 mgide %1 %2 %3 %4 %5 %6 %7 %8 %9
    goto END
 
 :CALL32
 
-   call %HG_ROOT%\buildapp.bat %HG_CLEAN% HB32 mgide  %1 %2 %3 %4 %5 %6 %7 %8 %9
+   call "%HG_ROOT%\BuildApp.bat" %HG_CLEAN% HB32 mgide  %1 %2 %3 %4 %5 %6 %7 %8 %9
    goto END
 
 :END
+
+   set HG_CLEAN=
+   set HG_START_DP_IDE_BUILD_BAT=
--- compile.bat
+++ compile.bat
@@ -3,59 +3,58 @@ rem
 rem $Id: compile.bat $
 rem
 
-:MAIN
+:COMPILE_IDE
 
-   if /I not "%1%"=="/NOCLS" cls
-   if /I not "%1%"=="/NOCLS" shift
+   pushd "%~dp0"
+   set HG_START_DP_IDE_COMPILE_BAT=%CD%
+   popd
 
-   if exist mgide.rc goto PATH
-   echo This file must be executed from IDE folder !!!
-   echo.
-   goto EXIT
+   if /I not "%1%" == "/NOCLS" cls
+   if /I "%1%" == "/NOCLS" shift
 
-:PATH
+   if not exist mgide.rc goto ERROR1
 
-   if /I "%1"=="/C" goto CLEAN_PATH
-   if "%HG_ROOT%"=="" set HG_ROOT=c:\oohg
-   if "%HG_HRB%"==""  set HG_HRB=c:\oohg\xhbcc
-   if "%HG_BCC%"==""  set HG_BCC=c:\Borland\BCC55
-   if "%LIB_GUI%"=="" set LIB_GUI=lib\xhb\bcc
-   if "%LIB_HRB%"=="" set LIB_HRB=lib
-   if "%BIN_HRB%"=="" set BIN_HRB=bin
-   goto CLEAN_EXE
-
-:ERREXIT1
-
-   echo COMPILE ERROR: is OIDE.EXE running ?
-   goto EXIT
-
-:CLEAN_PATH
-
-   set HG_ROOT=c:\oohg
-   set HG_HRB=c:\oohg\xhbcc
-   set HG_BCC=c:\Borland\BCC55
-   set LIB_GUI=lib\xhb\bcc
-   set LIB_HRB=lib
-   set BIN_HRB=bin
+   if /I not "%1" == "/C" goto ROOT
    shift
+   set HG_ROOT=
+   set HG_HRB=
+   set HG_BCC=
+   set LIB_GUI=
+   set LIB_HRB=
+   set BIN_HRB=
+
+:ROOT
+
+   if not "%HG_ROOT%" == "" goto CONTINUE
+   pushd "%HG_START_DP_IDE_COMPILE_BAT%\.."
+   set HG_ROOT=%CD%
+   popd
+
+:CONTINUE
+
+   if "%HG_HRB%"==""   set HG_HRB=%HG_ROOT%\xhbcc
+   if "%HG_BCC%"==""   set HG_BCC=c:\Borland\BCC55
+   if "%LIB_GUI%"==""  set LIB_GUI=lib\xhb\bcc
+   if "%LIB_HRB%"==""  set LIB_HRB=lib
+   if "%BIN_HRB%"==""  set BIN_HRB=bin
 
 :CLEAN_EXE
 
    if exist oide.exe del oide.exe
-   if exist oide.exe goto ERREXIT1
+   if exist oide.exe goto ERROR2
 
 :RESOURCES
 
    echo BRC32: Compiling resources...
    md auxdir
    cd auxdir
-   xcopy %HG_ROOT%\resources\*.* /q > nul
+   xcopy "%HG_ROOT%\resources\*.*" /q > nul
    md imgs
    cd imgs
    xcopy ..\..\imgs\*.* /q > nul
    cd ..
    copy /b oohg_bcc.rc + ..\mgide.rc oide.rc /y > nul
-   %HG_BCC%\bin\brc32 -r oide.rc > nul
+   "%HG_BCC%\bin\brc32.exe" -r oide.rc > nul
    copy oide.res .. /y > nul
    cd ..
    rd auxdir /s /q
@@ -65,17 +64,17 @@ rem
    set HG_DEFXHB=
    if /I not "%1"=="/H" set HG_DEFXHB=-D__XHARBOUR__
    echo Harbour: Compiling sources...
-   %HG_HRB%\%BIN_HRB%\harbour prgs\mgide    -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
-   %HG_HRB%\%BIN_HRB%\harbour prgs\dbucvc   -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
-   %HG_HRB%\%BIN_HRB%\harbour prgs\formedit -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
-   %HG_HRB%\%BIN_HRB%\harbour prgs\menued   -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
-   %HG_HRB%\%BIN_HRB%\harbour prgs\toolbed  -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
+   "%HG_HRB%\%BIN_HRB%\harbour.exe" prgs\mgide    -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
+   "%HG_HRB%\%BIN_HRB%\harbour.exe" prgs\dbucvc   -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
+   "%HG_HRB%\%BIN_HRB%\harbour.exe" prgs\formedit -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
+   "%HG_HRB%\%BIN_HRB%\harbour.exe" prgs\menued   -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
+   "%HG_HRB%\%BIN_HRB%\harbour.exe" prgs\toolbed  -i%HG_HRB%\include;%HG_ROOT%\include;fmgs -n1 -w3 -gc0 -es2 -q0
    echo BCC32: Compiling...
-   %HG_BCC%\bin\bcc32 -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% mgide.c    > nul
-   %HG_BCC%\bin\bcc32 -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% dbucvc.c   > nul
-   %HG_BCC%\bin\bcc32 -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% formedit.c > nul
-   %HG_BCC%\bin\bcc32 -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% menued.c   > nul
-   %HG_BCC%\bin\bcc32 -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% toolbed.c  > nul
+   "%HG_BCC%\bin\bcc32.exe" -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% mgide.c    > nul
+   "%HG_BCC%\bin\bcc32.exe" -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% dbucvc.c   > nul
+   "%HG_BCC%\bin\bcc32.exe" -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% formedit.c > nul
+   "%HG_BCC%\bin\bcc32.exe" -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% menued.c   > nul
+   "%HG_BCC%\bin\bcc32.exe" -c -O2 -tW -M -d -a8 -OS -5 -6 -w -I%HG_HRB%\include;%HG_BCC%\include;%HG_ROOT%\include; -L%HG_HRB%\%LIB_HRB%;%HG_BCC%\lib; %HG_DEFXHB% toolbed.c  > nul
 
 :LINK
 
@@ -87,22 +86,32 @@ rem
    echo %HG_ROOT%\%LIB_GUI%\oohg.lib + >> b32.bc
    for %%a in ( rtl vm gtgui lang codepage macro rdd dbfntx dbfcdx dbffpt common debug pp ct dbfdbt hbsix tip hsx xhb )      do if exist %HG_HRB%\%LIB_HRB%\%%a.lib echo %HG_HRB%\%LIB_HRB%\%%a.lib + >> b32.bc
    for %%a in ( hbrtl hbvm hblang hbcpage hbmacro hbrdd rddntx rddcdx rddfpt hbcommon hbdebug hbpp hbct hbwin pcrepos zlib ) do if exist %HG_HRB%\%LIB_HRB%\%%a.lib echo %HG_HRB%\%LIB_HRB%\%%a.lib + >> b32.bc
-   if exist %HG_HRB%\%LIB_HRB%\libmisc.lib    echo %HG_HRB%\%LIB_HRB%\libmisc.lib + >> b32.bc
-   if exist %HG_HRB%\%LIB_HRB%\hboleaut.lib   echo %HG_HRB%\%LIB_HRB%\hboleaut.lib + >> b32.bc
-   if exist %HG_HRB%\%LIB_HRB%\dll.lib        echo %HG_HRB%\%LIB_HRB%\dll.lib + >> b32.bc
-   if exist %HG_HRB%\%LIB_HRB%\socket.lib     echo %HG_HRB%\%LIB_HRB%\socket.lib + >> b32.bc
-   if exist %HG_ROOT%\%LIB_GUI%\socket.lib    echo %HG_ROOT%\%LIB_GUI%\socket.lib + >> b32.bc
-   if exist %HG_ROOT%\%LIB_GUI%\bostaurus.lib echo %HG_ROOT%\%LIB_GUI%\bostaurus.lib + >> b32.bc
-   if exist %HG_ROOT%\%LIB_GUI%\hbprinter.lib echo %HG_ROOT%\%LIB_GUI%\hbprinter.lib + >> b32.bc
-   if exist %HG_ROOT%\%LIB_GUI%\miniprint.lib echo %HG_ROOT%\%LIB_GUI%\miniprint.lib + >> b32.bc
+   if exist "%HG_HRB%\%LIB_HRB%\libmisc.lib"    echo %HG_HRB%\%LIB_HRB%\libmisc.lib + >> b32.bc
+   if exist "%HG_HRB%\%LIB_HRB%\hboleaut.lib"   echo %HG_HRB%\%LIB_HRB%\hboleaut.lib + >> b32.bc
+   if exist "%HG_HRB%\%LIB_HRB%\dll.lib"        echo %HG_HRB%\%LIB_HRB%\dll.lib + >> b32.bc
+   if exist "%HG_HRB%\%LIB_HRB%\socket.lib"     echo %HG_HRB%\%LIB_HRB%\socket.lib + >> b32.bc
+   if exist "%HG_ROOT%\%LIB_GUI%\socket.lib"    echo %HG_ROOT%\%LIB_GUI%\socket.lib + >> b32.bc
+   if exist "%HG_ROOT%\%LIB_GUI%\bostaurus.lib" echo %HG_ROOT%\%LIB_GUI%\bostaurus.lib + >> b32.bc
+   if exist "%HG_ROOT%\%LIB_GUI%\hbprinter.lib" echo %HG_ROOT%\%LIB_GUI%\hbprinter.lib + >> b32.bc
+   if exist "%HG_ROOT%\%LIB_GUI%\miniprint.lib" echo %HG_ROOT%\%LIB_GUI%\miniprint.lib + >> b32.bc
    echo cw32.lib + >> b32.bc
    echo msimg32.lib + >> b32.bc
    echo import32.lib, , + >> b32.bc
    echo oide.res + >> b32.bc
-   %HG_BCC%\bin\ilink32 -Gn -Tpe -aa -L%HG_BCC%\lib;%HG_BCC%\lib\psdk; @b32.bc > nul
+   "%HG_BCC%\bin\ilink32.exe" -Gn -Tpe -aa -L%HG_BCC%\lib;%HG_BCC%\lib\psdk; @b32.bc > nul
    if exist oide.exe goto OK
    echo Build finished with ERROR !!!
    goto CLEAN
+
+:ERROR1
+
+   echo This file must be executed from IDE folder !!!
+   goto END
+
+:ERROR2
+
+   echo COMPILE ERROR: Is oide.exe running ?
+   goto END
 
 :OK
 
@@ -118,6 +127,7 @@ rem
    for %%a in (*.res)  do del %%a
    set HG_DEFXHB=
 
-:EXIT
+:END
 
+   set HG_START_DP_IDE_COMPILE_BAT=
    echo.
